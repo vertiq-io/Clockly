d6e1884923454254d160720be16cadbc63288d7e
Manuel Grundner <m.grundner@delegate.at>
2023-10-05 14:08:18 +0200

    feat: added inline grid editing

--- src/Clockly.ShellModule/Pages/TimeTracking/TimePage.razor.cs
+++ src/Clockly.ShellModule/Pages/TimeTracking/TimePage.razor.cs
@@ -1,4 +1,6 @@
-﻿using Clockly.Models;
+﻿using System.Collections.Immutable;
+
+using Clockly.Models;
 
 using Fluxor;
 
@@ -15,22 +17,23 @@ public partial class TimePage
 {
     public sealed record TimeViewModel
     {
-        public IEnumerable<TimeEntry> TimeEntries { get; init; } = Enumerable.Empty<TimeEntry>();
+        public ImmutableArray<TimeEntry> TimeEntries { get; init; } = ImmutableArray.Create<TimeEntry>();
     }
 
     public sealed record TimeEntry(
-        int Id, 
-        string Description, 
-        DateOnly Date, 
-        TimeOnly Start, 
+        int Id,
+        string Description,
+        DateOnly Date,
+        TimeOnly Start,
         TimeOnly? End
     )
     {
-        public decimal Duration => 
+        public decimal Duration =>
             (Convert.ToDecimal((End.HasValue ? new TimeOnly(End.Value.Ticks - Start.Ticks) : new TimeOnly())
             .ToTimeSpan()
             .TotalMinutes) / 60m);
 
+        public bool EditMode { get; set; }
     }
 
     public sealed record FetchRequest(TimeViewModel? ViewModel = null) : Request<TimeViewModel>
@@ -60,9 +63,33 @@ public partial class TimePage
 
                 return new TimeViewModel
                 {
-                    TimeEntries = items
+                    TimeEntries = items.ToImmutableArray()
                 };
             }
+        }
+    }
+
+    public sealed record EditModeRequest(TimeViewModel ViewModel, TimeEntry Entry) : Request<TimeViewModel>
+    {
+        public sealed record Handler() : RequestHandler<EditModeRequest, TimeViewModel>
+        {
+            public override async ValueTask<Response<TimeViewModel>> Handle(EditModeRequest request, CancellationToken cancellationToken)
+            {
+                await Task.CompletedTask;
+
+                //var vm = request.ViewModel with
+                //{
+                //    TimeEntries = request.ViewModel.TimeEntries.Select(r => r with { EditMode = false }).ToImmutableArray()
+                //};
+
+                var vm = request.ViewModel with
+                {
+                    TimeEntries = request.ViewModel.TimeEntries.Replace(request.Entry, request.Entry with { EditMode = true })
+                };
+
+                return vm;
+            }
+
         }
     }
 
@@ -84,7 +111,34 @@ public partial class TimePage
             }
         }
     }
+    public sealed record SaveRequest(TimeEntry Entry) : Request<FetchRequest>
+    {
+        public sealed record Handler(IUnitOfWorkFactory UnitOfWorkFactory) : RequestHandler<SaveRequest, FetchRequest>
+        {
+            public override async ValueTask<Response<FetchRequest>> Handle(
+                SaveRequest request, 
+                CancellationToken cancellationToken
+            )
+            {
+                using var uow = await UnitOfWorkFactory.CreateUnitOfWork<CTimeEntry>();
+                var item = await uow.GetObjectByKeyAsync<CTimeEntry>(request.Entry.Id, cancellationToken);
+                if (item is null)
+                {
+                    return new FetchRequest();
+                }
 
+                item.Date = request.Entry.Date;
+                item.TimeFrom = request.Entry.Start;
+                item.TimeTo = request.Entry.End;
+                item.Description = request.Entry.Description;
+
+                await uow.SaveAsync(item, cancellationToken);
+                await uow.CommitChangesAsync(cancellationToken);
+                return new FetchRequest();
+            }
+        }
+    }
+    
     public sealed class EffectDelete : Effect<MediatorLoaded<DeleteRequest>>
     {
         public override Task HandleAsync(MediatorLoaded<DeleteRequest> action, IDispatcher dispatcher)
@@ -97,6 +151,15 @@ public partial class TimePage
     public sealed class EffectAdd : Effect<MediatorLoaded<AddRequest>>
     {
         public override Task HandleAsync(MediatorLoaded<AddRequest> action, IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch(new FetchRequest());
+            return Task.CompletedTask;
+        }
+    }
+
+    public sealed class EffectSave : Effect<MediatorLoaded<SaveRequest>>
+    {
+        public override Task HandleAsync(MediatorLoaded<SaveRequest> action, IDispatcher dispatcher)
         {
             dispatcher.Dispatch(new FetchRequest());
             return Task.CompletedTask;
